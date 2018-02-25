706870eebd4e72dd2bb254e6dd31fe0ee2410fc8
DESKTOP-EH993IN\soumithbsv <bsvsoumith1994@gmail.com>
2018-02-24 16:49:09 -0800

    Added initial FPS Game with scripted AI behaviour

---
+++ Source/TestingGrounds/NPC/ChooseNextWaypoint.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+#include "TestingGrounds.h"
+#include "AIController.h"
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) {
+
+	//Get Patrol points
+	auto AIController =OwnerComp.GetAIOwner();
+	auto ControlledPawn = AIController->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+
+	if (!ensure(PatrolRoute)) {
+		return EBTNodeResult::Failed;
+	}
+
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if(PatrolPoints.Num()==0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
+	//Set next waypoint
+	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
+	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]); //Sets WaypointKey to PatrolPoints[Index]
+
+	//Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num(); //PatrolPoints.Num gives the length of the array.
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex); //Sets IndexKey to NextIndex.
+
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint index: %i"), Index);
+	return EBTNodeResult::Succeeded;
+}
+
